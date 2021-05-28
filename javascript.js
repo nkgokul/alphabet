0cbedb8c39bb544be3a59e8ace13c35ae31128c0
Gokul NK <nkgokul@gmail.com>
2021-05-29 01:44:25 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const log = $('#log');
+    document.addEventListener('keyup', logKey);
+    msg = new SpeechSynthesisUtterance();
+    msg.text = "Abhirath type somethign on the keyboard";
+    window.speechSynthesis.speak(msg);
+
+    msgWhatLetter = new SpeechSynthesisUtterance();
+    msgWhatLetter.text = "Abhirath, what letter is this";
+});
+
+
+function logKey(e) {
+    let keyPressed = String.fromCharCode(e.keyCode);
+    if (keyPressed.match(/^[0-9a-zA-Z]+$/)) {
+        log.textContent = keyPressed;
+        $("#log").css("font-size", "70vh");
+        msg.text = "This is     " + keyPressed.toLowerCase();
+        document.removeEventListener('keyup', logKey);
+        window.speechSynthesis.speak(msgWhatLetter);
+        setTimeout(function () { window.speechSynthesis.speak(msg); document.addEventListener('keyup', logKey); }, 5000);
+    }
+}
